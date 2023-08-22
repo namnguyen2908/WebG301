4286cfb2fed1746f0cf0608b1e6292328eb77dc6
namnguyen2908 <namnpgch211315@fpt.edu.vn>
2023-08-22 09:44:45 +0700

    tạo model category

--- Music/resources/views/authors/show.blade.php
+++ Music/resources/views/authors/show.blade.php
@@ -12,7 +12,6 @@
                                 <div class="price d-flex flex-row align-items-center">
                                     <div class="ml-2"> <small class="dis-price">Email: </small> <span>{{$author->email}}</span> </div>
                                 </div>
-                                
                             </div>
                             <p class="about">Shop from a wide range of t-shirt from orianz. Pefect for your everyday use, you could pair it with a stylish pair of jeans or trousers complete the look.</p>
                             <div class="cart mt-4 align-items-center"> <button class="btn btn-danger text-uppercase mr-2 px-4">Dowload</button> <i class="fa fa-heart text-muted"></i> <i class="fa fa-share-alt text-muted"></i> </div>
